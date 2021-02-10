ddf7aa5608dfc87c2544cce1a4693656aa3ce6ed
Zther <Zther@Zther-Dsk>
2021-02-10 12:20:13 +0100

    Editado por Ezequiel

--- src/hola/Hola.java
+++ src/hola/Hola.java
@@ -5,5 +5,7 @@ public class Hola {
 	public static void main(String args[]) {
 		
 		System.out.println("Hola, soy Alvaro");
+		System.out.println("Hola, soy Ezequiel");
+		System.out.println("asd");
 	}
 }
